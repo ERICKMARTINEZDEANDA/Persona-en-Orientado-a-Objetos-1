0ef044a45e8de003db2d8b3a90483c616037a66b
ERICKMARTINEZDEANDA <andy1pingui1@gmail.com>
2025-08-14 14:38:22 -0600

    Update Persona 1.py
    Uso de parametros especiales

--- Persona 1.py
+++ Persona 1.py
@@ -1,15 +1,15 @@
 class Persona:
 	#Constructor
-	def __init__(self): #self es requerido para entrar a los metodos dentro de la clase
-		self.edad=18 #atributos
-		self.nombre="Juan"
+	def __init__(self, edad, nombre): #self es requerido para entrar a los metodos dentro de la clase
+		self.edad=edad #atributos
+		self.nombre=nombre
 		print("se ha creado a", self.nombre, "de", self.edad, "años")
 
 	#metodos
 	def hablar (self, palabras = "No tengo palabras"): #Se puede colocar un valor predeterminado
 		print(self.nombre, ": ", palabras)
 
-juan = Persona()
+juan = Persona(18, "Juan")
 juan.hablar()
 juan.hablar("hola, estoy hablando")
 
